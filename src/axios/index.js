b00913e30b5aea3efc6211dff746124bffbb61d4
Joao Pedro Favara <joaopedro_favara@hotmail.com>
2020-02-18 21:30:22 -0300

    Front 2

---
+++ src/axios/index.js
@@ -0,0 +1,3 @@
+import Axios from Axios;
+
+module.exports = Axios;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,21 +2,35 @@
   <div class="box-login">
     <div class="box-input">
         <span>LOGIN:</span>
-        <input class="input" type="text" placeholder="Text input">
+        <input class="input" v-model="login" type="text" placeholder="Text your username">
     </div>
     <div class="box-input">
-        <span>SENHA:</span>
-        <input class="input" type="text" placeholder="Text input">
+        <span>PASSWORD:</span>
+        <input class="input" v-model="password" type="text" placeholder="Text your password">
     </div>
-
-    <button class="button">Button</button>
+    <div class="box-button">
+      <button v-bind:class="{ 'is-loading': this.authentication }" class="button is-success" v-on:click="this.teste">Confirm</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
-
+  data() {
+    return {
+      password: null,
+      login: null,
+      authentication: false,
+    }
+  },
+  methods: {
+    teste() {
+      this.authentication = true;
+      console.log(this.password);
+      console.log(this.login);
+    }
+  },
 };
 </script>
 
@@ -27,22 +41,23 @@ export default {
     top: 20%;
     left: 30%;
     width: 40%;
-    border: 1px red solid;
+    border: 1px green dashed;
     padding: 20px;
     
     .box-input {
       display: flex;
       align-items: center;
-      justify-content: flex-end;
+      justify-content: flex-start;
       margin: 5px;
 
       span {
         margin-right: 1px;
       }
+    }
 
-      input {
-        width: 250px;
-      }
+    .box-button {
+      display: flex;
+      justify-content: flex-end;
     }
   }
 
